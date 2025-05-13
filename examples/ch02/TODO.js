62a1b5b4ea7e593ac97d5ebe4dca3ed2978ae9e0
Konstantin <drpdrp@mail.ru>
2025-05-14 02:17:33 +0300

    CHG: crossing out fullfilled tasks

--- examples/ch02/TODO.js
+++ examples/ch02/TODO.js
@@ -1,4 +1,8 @@
-const todos = ['Walk the dog', 'Water the plants', 'Sand the chairs' ]
+const todos = [
+  { description: 'Walk the dog', done: false },
+  { description: 'Water the plants', done: false },
+  { description: 'Sand the chairs', done: false },
+]
 
 const addTodoInput = document.getElementById('todo-input')
 const addTodoButton = document.getElementById('add-todo-btn')
@@ -18,31 +22,42 @@ addTodoInput.addEventListener('keydown', ({ key }) => {
     }
 })
 
-addTodoButton.addEventListener('click', ()=>{
-    addTodo();
-})
+addTodoButton.addEventListener('click', () => { addTodo(); })
 
 function renderTodoInReadMode(todo) {
     const li = document.createElement('li');
     
     const span = document.createElement('span');
-    span.textContent = todo;
-    span.addEventListener('dblclick', () => {
-        const idx = todos.indexOf(todo);
-        todosList.replaceChild(
-            renderTodoInEditMode(todo),
-            todosList.childNodes[idx]
-        )
-    });
+    span.textContent = todo.description;
+
+    console.log(todo)
+    if (todo.done) { 
+        span.classList.add(`done`);
+    }
+    else{
+        span.addEventListener('dblclick', () => { // -> edit
+            const idx = todos.indexOf(todo);
+            todosList.replaceChild(
+                renderTodoInEditMode(todo),
+                todosList.childNodes[idx]
+            )
+        });
+    }
     li.append(span);
 
-    const button = document.createElement('button');
-    button.textContent = 'Done';
-    button.addEventListener('click', () => {
-        const idx = todos.indexOf(todo);
-        removeTodo(idx);
-    });
-    li.append(button);
+    if (!todo.done) {
+        const button = document.createElement('button');
+        button.textContent = 'Done';
+        button.addEventListener('click', () => {
+            const idx = todos.indexOf(todo);
+            removeTodo(idx);
+            todosList.replaceChild(
+                renderTodoInReadMode(todo),
+                todosList.childNodes[idx]
+            )
+        });
+        li.append(button);
+    }
 
     return li;
 }
@@ -52,7 +67,7 @@ function renderTodoInEditMode(todo) {
     
     const input = document.createElement('input');
     input.type = 'text';
-    input.value = todo;
+    input.value = todo.description;
     li.append(input);
 
     const saveButton = document.createElement('button');
@@ -78,21 +93,19 @@ function renderTodoInEditMode(todo) {
 }
 
 function addTodo() {
-    const description = addTodoInput.value;
-    todos.push(description);
-    todosList.append(renderTodoInReadMode(description))
+    const newTodo = { description:addTodoInput.value, done:false };
+    todos.push(newTodo);
+    todosList.append(renderTodoInReadMode(newTodo))
     addTodoInput.value = ``;
     addTodoButton.disabled = true;
 }
 
 function updateTodo(idx, description) {
-    todos[idx] = description
+    const newTodo = { description:description, done:false };
+    todos[idx] = newTodo;
     todosList.replaceChild(
-        renderTodoInReadMode(description),
+        renderTodoInReadMode(newTodo),
         todosList.childNodes[idx])
 }
 
-function removeTodo(idx) {
-    todos.splice(idx, 1);
-    todosList.children[idx].remove(); // remove node    
-}
+function removeTodo(idx) { todos[idx].done = true; }
